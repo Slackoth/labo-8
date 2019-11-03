1841bada030fcad0e0d78619a009e625a53888d5
Slackoth <00082318@uca.edu.sv>
2019-11-03 00:27:52 -0600

    Updates

---
+++ precache-manifest.77a8b8aec112abcbca74cf4c64a427a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d64153ca8e5c8529940938aa4d53e9c",
+    "url": "/labo-8/index.html"
+  },
+  {
+    "revision": "56a4feaedf0bd54da065",
+    "url": "/labo-8/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8c117aea068efa3808f7",
+    "url": "/labo-8/static/js/2.7dee1f1b.chunk.js"
+  },
+  {
+    "revision": "56a4feaedf0bd54da065",
+    "url": "/labo-8/static/js/main.9435633c.chunk.js"
+  },
+  {
+    "revision": "c168f94b50e471799520",
+    "url": "/labo-8/static/js/runtime-main.de191d81.js"
+  }
+]);
